f1fc4eacfb3626550ec858ba52db73b882a9b46e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-01 12:12:13 -0800

    finished default screen

---
+++ screens/DefaultScreen.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+function DefaultScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Smart Trading Technology</Text>
+      <Text style={styles.text}>
+        EvergreenFX intelligent bot eliminates all the stress and hassle of
+        traditional forex trading.
+      </Text>
+      <Image style={styles.img} source={require("../assets/circle.png")} />
+      <View style={styles.loginParent}>
+        <TouchableOpacity style={styles.login}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.loginParent}>
+        <TouchableOpacity
+          style={[styles.login, { backgroundColor: "#00C805" }]}
+        >
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default DefaultScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFAFA",
+    padding: 10,
+  },
+  headerText: {
+    fontSize: 40,
+    fontWeight: "700",
+    marginTop: 40,
+  },
+  img: {
+    position: "relative",
+    right: -20,
+  },
+  login: {
+    backgroundColor: "#FFFFFF",
+    width: "90%",
+    height: 43,
+    borderRadius: 12,
+    justifyContent: "center",
+    borderColor: "#191919",
+    borderWidth: 0.5,
+    shadowColor: "red",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 3,
+  },
+  loginParent: {
+    width: "100%",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  loginText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "400",
+    marginTop: 5,
+  },
+});
